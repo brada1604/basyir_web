2bd12caf529c073eb73ab9bbd485b352caf561b8
brada1604 <bagusnugroho1604@gmail.com>
2023-05-06 13:27:44 +0700

    upload file

--- app/Views/home/v_home.php
+++ app/Views/home/v_home.php
@@ -1,5 +1,5 @@
         <tr align="center">
 			<td colspan="5">
-				SELAMAT DATANG <?= $session->get('name');?> - <?= $session->get('nip');?>
+				SELAMAT DATANG di web <?= $session->get('name');?> - <?= $session->get('nip');?>
 			</td>
 		</tr>
